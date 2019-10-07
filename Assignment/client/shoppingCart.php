b7fa30b383e0dbc3ef116cc25a1292f281ffd3a2
rmit-s3711351-Bojian-KE <s3711351@student.rmit.edu.au>
2019-10-07 12:21:42 +1100

    updaet assignment

--- Assignment/client/shoppingCart.php
+++ Assignment/client/shoppingCart.php
@@ -1,4 +1,14 @@
-<!DOCTYPE html>
+<?php
+
+session_start();
+
+if(!isset($_SESSION['login'])){
+    header('Location: login.html');
+}
+    
+?>
+
+
 <html>
 
 <head>
@@ -30,7 +40,7 @@
                 <td width="29%"><input type="hidden" id="aname" name="aname" value="producta">Product A</td>
                 <td width="17%"><input type="hidden" id="aprice" name="aprice" value="10">$10</td>
                 <td width="30%"><input min="0" type="number" id="aquantity" name="aquantity" style="width:30px"
-                        value="0"></input></td>
+                        value="0"></td>
                 <td>
                     <p id="atotal"> 0</p>
                     <input type="hidden" id="asubtotal" name="asubtotal" value=0>
@@ -40,7 +50,7 @@
             <tr>
                 <td><input type="hidden" id="bname" name="bname" value="productb">Product B</td>
                 <td><input type="hidden" id="bprice" name="bprice" value="15">$15</td>
-                <td><input min="0" type="number" id="bquantity" name="bquantity" style="width:30px" value="0"></input>
+                <td><input min="0" type="number" id="bquantity" name="bquantity" style="width:30px" value="0">
                 </td>
                 <td>
                     <p id="btotal"> 0</p>
@@ -50,7 +60,7 @@
             <tr>
                 <td><input type="hidden" id="cname" name="cname" value="productc">Product C</td>
                 <td><input type="hidden" id="cprice" name="cprice" value="20">$20</td>
-                <td><input min="0" type="number" id="cquantity" name="cquantity" style="width:30px" value="0"></input>
+                <td><input min="0" type="number" id="cquantity" name="cquantity" style="width:30px" value="0">
                 </td>
                 <td>
                     <p id="ctotal"> 0</p>
@@ -90,6 +100,34 @@
         </table>
 
     </FORM>
+
+    <script>
+
+
+        function update() {
+            var atotal = document.getElementById("aquantity").value * 10;
+            var btotal = document.getElementById("bquantity").value * 15;
+            var ctotal = document.getElementById("cquantity").value * 20;
+            var quantity = Number(document.getElementById("aquantity").value) + Number(document.getElementById("bquantity").value) + Number(document.getElementById("cquantity").value);
+            var total = Number(atotal) + Number(btotal) + Number(ctotal);
+
+            document.getElementById("atotal").innerHTML = atotal;
+            document.getElementById("asubtotal").value = atotal;
+
+            document.getElementById("btotal").innerHTML = btotal;
+            document.getElementById("bsubtotal").value = btotal;
+
+            document.getElementById("ctotal").innerHTML = ctotal;
+            document.getElementById("csubtotal").value = ctotal;
+
+            document.getElementById("totalquantity").innerHTML = quantity;
+            document.getElementById("totalnumber").value = quantity;
+
+            document.getElementById("total").innerHTML = total;
+            document.getElementById("totalvalue").value = total;
+        }
+
+    </script>
 
 </body>
 
