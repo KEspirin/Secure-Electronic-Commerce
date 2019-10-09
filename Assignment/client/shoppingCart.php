c3f8311a0cc212633acc2fb7eafce6ddfb8f35b0
KEspirin <s3711351@student.rmit.edu.au>
2019-10-10 00:46:37 +1100

    wip shoppingcart

--- Assignment/client/shoppingCart.php
+++ Assignment/client/shoppingCart.php
@@ -39,31 +39,38 @@ if (!isset($_SESSION['login'])) {
             <tr>
                 <td><input type="hidden" id="aname" name="aname" value="producta">Product A</td>
                 <td><input type="hidden" id="aprice" name="aprice" value="10">$10</td>
-                <td><input min="0" type="number" id="aquantity" name="aquantity" style="width:30px" value="0"></td>
+                <td>
+                    <input min="0" type="number" id="aquantity" name="aquantity" style="width:30px" value="0">
+                    <input type="hidden" id="aquantityenc" name="aquantityenc" value=0>
+                </td>
                 <td>
                     <p id="atotal"> 0</p>
-                    <input type="hidden" id="asubtotal" name="asubtotal" value=0>
+                    <input type="hidden" id="asubtotalenc" name="asubtotalenc" value=0>
                 </td>
             </tr>
 
             <tr>
                 <td><input type="hidden" id="bname" name="bname" value="productb">Product B</td>
                 <td><input type="hidden" id="bprice" name="bprice" value="15">$15</td>
-                <td><input min="0" type="number" id="bquantity" name="bquantity" style="width:30px" value="0">
+                <td>
+                    <input min="0" type="number" id="bquantity" name="bquantity" style="width:30px" value="0">
+                    <input type="hidden" id="bquantityenc" name="bquantityenc" value=0>
                 </td>
                 <td>
                     <p id="btotal"> 0</p>
-                    <input type="hidden" id="bsubtotal" name="bsubtotal" value=0>
+                    <input type="hidden" id="bsubtotalenc" name="bsubtotalenc" value=0>
                 </td>
             </tr>
             <tr>
                 <td><input type="hidden" id="cname" name="cname" value="productc">Product C</td>
                 <td><input type="hidden" id="cprice" name="cprice" value="20">$20</td>
-                <td><input min="0" type="number" id="cquantity" name="cquantity" style="width:30px" value="0">
+                <td>
+                    <input min="0" type="number" id="cquantity" name="cquantity" style="width:30px" value="0">
+                    <input type="hidden" id="cquantityenc" name="cquantityenc" value=0>
                 </td>
                 <td>
                     <p id="ctotal"> 0</p>
-                    <input type="hidden" id="csubtotal" name="csubtotal" value=0>
+                    <input type="hidden" id="csubtotalenc" name="csubtotalenc" value=0>
                 </td>
             </tr>
 
@@ -74,11 +81,26 @@ if (!isset($_SESSION['login'])) {
                 <td></td>
                 <td>
                     <p id="totalquantity">0</p>
-                    <input type="hidden" id="totalnumber" name="totalnumber" value=0>
+                    <input type="hidden" id="totalnumberenc" name="totalnumberenc" value=0>
                 </td>
                 <td>
                     <p id="total">0</p>
-                    <input type="hidden" id="totalvalue" name="totalvalue" value=0>
+                    <input type="hidden" id="totalvalueenc" name="totalvalueenc" value=0>
+                </td>
+            </tr>
+
+            <tr></tr>
+
+            <tr>
+                <td>Your DES Key:</td>
+                <td colspan="3"><textarea id="DES_Encryption_Key" name="DES_Encryption_Key" placeholder="Please Enter Your DES Key Here" cols="50" rows="1" style="resize:none"></textarea></td>
+            </tr>
+
+            <tr>
+                <td>Credit Card Number:</td>
+                <td colspan="3">
+                    <textarea id="creditCard" name="creditCard" placeholder="Credit Card info" cols="50" rows="1" style="resize:none"></textarea>
+                    <input type="hidden" id="creditCardenc" name="creditCardenc" value=0>
                 </td>
             </tr>
 
@@ -92,7 +114,23 @@ if (!isset($_SESSION['login'])) {
 
     </FORM>
 
-    <script>
+    <script type="text/javascript" src="js/des.js"></script>
+    <script type="text/javascript" src="js/rsa.js"></script>
+    <script type="text/javascript">
+        // call the encryption methods, order matters
+        function encryption(){
+            DES_encryption("aquantity");
+            DES_encryption("bquantity");
+            DES_encryption("cquantity");
+            DES_encryption("asubtotal");
+            DES_encryption("bsubtotal");
+            DES_encryption("csubtotal");
+            DES_encryption("creditCard");
+
+            RSA_encryption();
+        }
+
+        // to automaticaly update the shopping cart quantity and price
         function update() {
             var atotal = document.getElementById("aquantity").value * 10;
             var btotal = document.getElementById("bquantity").value * 15;
@@ -115,6 +153,32 @@ if (!isset($_SESSION['login'])) {
             document.getElementById("total").innerHTML = total;
             document.getElementById("totalvalue").value = total;
         }
+
+        function DES_encryption(id) {
+            let plaintext = document.getElementById(id).value;
+            var key = document.getElementById("DES_Encryption_Key").value;
+
+            // javascript des encryption api
+            var encrypted = javascript_des_encryption(key, message);
+
+            document.getElementById(id+"enc").value = encrypted;
+
+        }
+
+        function RSA_encryption() {
+
+            var DES_Encryption_Key = document.getElementById("DES_Encryption_Key").value;
+            var pubilc_key = "-----BEGIN PUBLIC KEY-----MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAzdxaei6bt/xIAhYsdFdW62CGTpRX+GXoZkzqvbf5oOxw4wKENjFX7LsqZXxdFfoRxEwH90zZHLHgsNFzXe3JqiRabIDcNZmKS2F0A7+Mwrx6K2fZ5b7E2fSLFbC7FsvL22mN0KNAp35tdADpl4lKqNFuF7NT22ZBp/X3ncod8cDvMb9tl0hiQ1hJv0H8My/31w+F+Cdat/9Ja5d1ztOOYIx1mZ2FD2m2M33/BgGY/BusUKqSk9W91Eh99+tHS5oTvE8CI8g7pvhQteqmVgBbJOa73eQhZfOQJ0aWQ5m2i0NUPcmwvGDzURXTKW+72UKDz671bE7YAch2H+U7UQeawwIDAQAB-----END PUBLIC KEY-----";
+            // Encrypt with the public key...
+            var encrypt = new JSEncrypt();
+            encrypt.setPublicKey(pubilc_key);
+            var encrypted = encrypt.encrypt(DES_Encryption_Key);
+
+            document.getElementById("DES_Encryption_Key").value = encrypted;
+        }
+
+
+
     </script>
 
 </body>
